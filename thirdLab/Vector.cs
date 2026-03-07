ee52ac787f68ec5fb3d1d5c313b324048cdc09b8
MoonSneg <gryznov.gr@gmail.com>
2026-03-07 18:59:28 +0800

    add class Vector, functions of basic operations and tests for func

--- thirdLab/Vector.cs
+++ thirdLab/Vector.cs
@@ -12,13 +12,36 @@ namespace thirdLab
         private double y;
         private double z;
 
-        public Vector(double x, double y, double z)
-        {
+        public Vector(double x=0, double y=0, double z=0) {
             this.x = x;
             this.y = y;
             this.z = z;
         }
 
+        public string Show() {
+            return String.Format("Вектор: ({0}, {1}, {2})", this.x, this.y, this.z);
+        }
 
+        public static Vector operator+(Vector a, Vector b) {
+            return new Vector(a.x+b.x, a.y+b.y, a.z+b.z);
+        }
+
+        public static Vector operator-(Vector a, Vector b) {
+            return new Vector(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+        public static Vector operator *(Vector a, Vector b) {
+            return new Vector(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+
+        }
+
+        public static string ScalarMul(Vector a, Vector b) {
+            return String.Format("Скалярное произведение: {0}", a.x * b.x + a.y * b.y + a.z * b.z);
+        }
+        public string CalcLength() {
+            double length = Math.Sqrt(Math.Pow(this.x, 2) + Math.Pow(this.y, 2) + Math.Pow(this.z, 2));
+            return String.Format("Длина вектора: {0}", length);
+        }
+        
     }
 }
---
+++ thirdLabTests/VectorTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using thirdLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thirdLab.Tests
+{
+    [TestClass()]
+    public class VectorTests
+    {
+        [TestMethod()]
+        public void ShowTest()
+        {
+            var Vector1 = new Vector(3, 5, 7);
+            Assert.AreEqual("Вектор: (3, 5, 7)", Vector1.Show());
+        }
+
+
+        [TestMethod()]
+        public void AddNumbersTest()
+        {
+            var Vector1 = new Vector(3, 5, 7);
+            var Vector2 = new Vector(1, 2, 3);
+            var Vector0 = Vector1 + Vector2;
+            Assert.AreEqual("Вектор: (4, 7, 10)", Vector0.Show());
+        }
+
+        [TestMethod()]
+        public void SubNumbersTest()
+        {
+            var Vector1 = new Vector(3, 5, 7);
+            var Vector2 = new Vector(1, 2, 3);
+            var Vector0 = Vector1 - Vector2;
+            Assert.AreEqual("Вектор: (2, 3, 4)", Vector0.Show());
+        }
+
+        [TestMethod()]
+        public void VecMulByNumbersTest()
+        {
+            var Vector1 = new Vector(3, 5, 7);
+            var Vector2 = new Vector(1, 2, 3);
+            var Vector0 = Vector1 * Vector2;
+            Assert.AreEqual("Вектор: (1, -2, 1)", Vector0.Show());
+        }
+
+        [TestMethod()]
+        public void ScalarMulByNumbersTest()
+        {
+            var Vector1 = new Vector(3, 5, 7);
+            var Vector2 = new Vector(1, 2, 3);
+            Assert.AreEqual("Скалярное произведение: 34", Vector.ScalarMul(Vector1, Vector2));
+        }
+
+        [TestMethod()]
+        public void CalculationLengthTest()
+        {
+            var Vector1 = new Vector(3, 4, 0);
+            Assert.AreEqual("Длина вектора: 5", Vector1.CalcLength());
+        }
+
+      
+    }
+}
