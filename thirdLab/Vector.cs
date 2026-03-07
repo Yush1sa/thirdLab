d88baa5f7fa2096a04997d39b9e2615ca917f3b9
MoonSneg <gryznov.gr@gmail.com>
2026-03-07 17:57:23 +0800

    Добавьте файлы проекта.

---
+++ thirdLab/Vector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thirdLab
+{
+    public class Vector
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+
+    }
+}
